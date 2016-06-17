18428f327139ae189f933c9b567550d10d414e4e
Scott Cunningham <scottcunningham92@gmail.com>
2016-06-17 11:03:50 +0200

    Custom keymap + LED code

--- keymap_plain.c
+++ keymap_plain.c
@@ -16,14 +16,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "keymap_common.h"
 
+#include "led_controller.h"
+
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
      * ,---------------------------------------------------------------.
-     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|Ins|
+     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|Hom|
      * |---------------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|Del|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|End|
      * |---------------------------------------------------------------|
-     * |CapsLo|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU|
+     * | Fn0  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU|
      * |---------------------------------------------------------------|
      * |Shif|   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up |PgD|
      * |---------------------------------------------------------------|
@@ -31,13 +33,50 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     [0] = KEYMAP( \
-        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, INS, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,     DEL, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NUHS,ENT,      PGUP,\
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, HOME, \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,     END, \
+        FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NUHS,ENT,      PGUP,\
         LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,     UP,  PGDN,\
-        LCTL,LGUI,LALT,               SPC,           RALT,RGUI,RCTL,     LEFT,DOWN,RGHT \
+        LCTL,LGUI,LALT,               SPC,           RALT,RGUI,FN1,     LEFT,DOWN,RGHT \
+    ),
+    /* Layer 1: FN0 layer
+     * ,---------------------------------------------------------------.
+ F F * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  \|  `|Hom|
+     * |---------------------------------------------------------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|End|
+     * |---------------------------------------------------------------|
+     * | Fn0  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU|
+     * |---------------------------------------------------------------|
+     * |Shif|   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up |PgD|
+     * |---------------------------------------------------------------|
+     * |Ctrl|Gui |Alt |         Space    |Fn0 |Alt |Gui |  |Lef|Dow|Rig|
+     * `---------------------------------------------------------------'
+     */
+    [1] = KEYMAP( \
+        ESC,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10,   F11,F12, BSLS,GRV, HOME, \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,     END, \
+        FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NUHS,ENT,      PGUP,\
+        LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,     UP,  PGDN,\
+        LCTL,LGUI,LALT,               SPC,           RALT,FN1,NO ,     LEFT,DOWN,RGHT \
     ),
 };
 
+#define ACTION_LEDS_ALL 2
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+  (void)opt;
+  switch(id) {
+    case ACTION_LEDS_ALL:
+      // toggle enter LED on press
+      if(record->event.pressed) {
+        // signal the LED controller thread
+        chMBPost(&led_mailbox, LED_MSG_ALL_TOGGLE, TIME_IMMEDIATE);
+      }
+      break;
+  }
+}
+
 const uint16_t fn_actions[] = {
+    [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_FUNCTION(ACTION_LEDS_ALL),
 };
